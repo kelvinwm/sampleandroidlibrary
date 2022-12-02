f120b3a422f930d124aa04c513329d19227d24f1
kelvinwm <kelvinwmblogs@gmail.com>
2022-12-02 11:30:05 +0300

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Toaster.java
@@ -0,0 +1,13 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class Toaster {
+
+    public static void s(Context context, String url) {
+        Intent myIntent = new Intent(context, MyWebViewMainActivity.class);
+        myIntent.putExtra("redirectUrl", url);
+        context.startActivity(myIntent);
+    }
+}
