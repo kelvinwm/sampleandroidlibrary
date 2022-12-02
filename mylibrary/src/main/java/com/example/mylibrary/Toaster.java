c6e161f407e62567d8b8e1f2906e62c1556088b2
kelvinwm <kelvinwmblogs@gmail.com>
2022-12-02 11:38:07 +0300

    second commit

--- mylibrary/src/main/java/com/example/mylibrary/Toaster.java
+++ mylibrary/src/main/java/com/example/mylibrary/Toaster.java
@@ -7,6 +7,7 @@ public class Toaster {
 
     public static void s(Context context, String url) {
         Intent myIntent = new Intent(context, MyWebViewMainActivity.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         myIntent.putExtra("redirectUrl", url);
         context.startActivity(myIntent);
     }
